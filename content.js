49ca874b37065075b2ccba876a6c9530d73a216b
Brian Briscoe <brianbriscoe06@gmail.com>
2015-11-12 13:51:40 +0000

    Added case sensitivity function

--- content.js
+++ content.js
@@ -1,9 +1,19 @@
 //content.js
 
+// http://stackoverflow.com/questions/8746882/jquery-contains-selector-uppercase-and-lower-case-issue
+jQuery.expr[':'].containsInsensitive = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
 $(document).ready(function() {
-	$('div[data-testid*="fbfeed_story"]:contains("Jenner")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Jenner")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Kardashian")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Mr Cian Twomey")').hide();
 });
 
 $(window).scroll(function () {
-	$('div[data-testid*="fbfeed_story"]:contains("Jenner")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Jenner")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Kardashian")').hide();
+	$('div[data-testid*="fbfeed_story"]:containsInsensitive("Mr Cian Twomey")').hide();
 });
